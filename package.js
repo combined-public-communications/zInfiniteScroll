d5533e1e0b07bfc0d1dd008eb2a1dd17002df673
Zam <love8879201@gmail.com>
2016-05-17 10:01:08 +0800

    1.1.4

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: "lightzam:z-infinite-scroll",
     summary: "angular directive to handle element scroll",
     description: "angular directive to handle element scroll",
-    version: "1.1.3",
+    version: "1.1.4",
     git: "https://github.com/LightZam/zInfiniteScroll.git"
 });
 
